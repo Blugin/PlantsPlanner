3ef6aabeeb3afe272e86ee29134707337cc3c11e
PresentKim <debe3721@gmail.com>
2021-02-18 18:15:21 +0900

    PumpkinStemPlants : Add missing imports

--- src/kim/present/plantsplaner/block/PumpkinStemPlants.php
+++ src/kim/present/plantsplaner/block/PumpkinStemPlants.php
@@ -3,8 +3,8 @@ declare(strict_types=1);
 
 namespace kim\present\plantsplaner\block;
 
+use kim\present\plantsplaner\data\DefaultPlants;
 use kim\present\plantsplaner\data\PlantsData;
-use kim\present\plantsplaner\Loader;
 use kim\present\plantsplaner\traits\StemPlantsTrait;
 use pocketmine\block\MelonStem;
 
